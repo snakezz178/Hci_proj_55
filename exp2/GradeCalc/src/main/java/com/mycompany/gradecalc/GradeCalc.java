c8f5120fb37f35be2a296b3f71e5b519950b312f
Sneha <141407291+snakezz178@users.noreply.github.com>
2024-06-08 23:43:26 +0530

    Add files via upload

---
+++ exp2/GradeCalc/src/main/java/com/mycompany/gradecalc/GradeCalc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradecalc;
+
+/**
+ *
+ * @author ipssh
+ */
+public class GradeCalc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ exp3/SortNumbers/src/main/java/com/mycompany/sortnumbers/SortNumbers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sortnumbers;
+
+/**
+ *
+ * @author ipssh
+ */
+public class SortNumbers {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
